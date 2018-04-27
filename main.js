e6a063a68bbe1744ee1121ffe969feaae366b280
dkim22 <ehddnr1186@gamil.com>
2018-04-28 02:47:06 +0900

    CRA

--- main.js
+++ main.js
@@ -6,19 +6,43 @@ const { app, BrowserWindow } = electron;
 
 let mainWindow;
 
-function createWindow() {
+const createWindow = () => {
   mainWindow = new BrowserWindow({
     width: 800,
-    height: 600
+    height: 600,
+    resizable: false,
+    title: "Dkimcoin Wallet"
   });
 
-  mainWindow.loadURL(
-    url.format({
-      pathname: path.join(__dirname, "index.html"),
-      protocol: "file",
-      slashes: true
-    })
-  );
-}
+  const ENV = process.env.ENV;
+
+  if (ENV === "dev") {
+    mainWindow.loadURL("http://localhost:3000");
+  } else {
+    mainWindow.loadURL(
+      url.format({
+        pathname: path.join(__dirname, "uidev/build/index.html"),
+        protocol: "file",
+        slashes: true
+      })
+    );
+  }
+
+  mainWindow.on("closed", () => {
+    mainWindow = null;
+  });
+};
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+
+app.on("activate", () => {
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
 
 app.on("ready", createWindow);
