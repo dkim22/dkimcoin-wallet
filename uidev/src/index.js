c1487dfd836e8c09ed7c891c35c6bd822066b56b
dkim22 <ehddnr1186@gamil.com>
2018-04-29 20:08:43 +0900

    Bug fixing and Getting Address From Local Node

--- uidev/src/index.js
+++ uidev/src/index.js
@@ -1,11 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "Components/App";
-
 const { remote } = window.require("electron");
 
 const sharedPort = remote.getGlobal("sharedPort");
 
-window.sharedPort = sharedPort;
-
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<App sharedPort={sharedPort} />, document.getElementById("root"));
