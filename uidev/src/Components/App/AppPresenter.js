3431db32aac29b89d4fd9c07d785ead4bac14459
dkim22 <ehddnr1186@gamil.com>
2018-05-01 22:38:47 +0900

    Mining from the Wallet

--- uidev/src/Components/App/AppPresenter.js
+++ uidev/src/Components/App/AppPresenter.js
@@ -1,6 +1,15 @@
 import React from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components";
+import { Card, Key, KeyName, Title, Notification, Button } from "Components/Shared";
+
+const Header = styled.div`
+  margin: 50px 0;
+  width: 90%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
 
 const AppContainer = styled.div`
   background-color: #f2f6fa;
@@ -10,6 +19,31 @@ const AppContainer = styled.div`
   flex-direction: column;
 `;
 
-const AppPresenter = ({ isLoading }) => <AppContainer />;
+const AppPresenter = ({ isLoading, address = "", balance = "", mineBlock, isMining }) => (
+  <AppContainer>
+    <Header>
+      <Title>{isLoading ? "Loading..." : "Dkimcoin Wallet"}</Title>
+      <Button disabled={isMining} onClick={mineBlock}>
+        {isMining ? "Mining" : "Mine"}
+      </Button>
+    </Header>
+    <Card>
+      <Key>
+        <KeyName>Your address:</KeyName> <Key>{address}</Key>
+      </Key>
+      <Key>
+        <KeyName>Your balance:</KeyName> {balance} DKC
+      </Key>
+    </Card>
+  </AppContainer>
+);
+
+AppPresenter.propTypes = {
+  isLoading: PropTypes.bool.isRequired,
+  address: PropTypes.string,
+  balance: PropTypes.number,
+  mineBlock: PropTypes.func.isRequired,
+  isMining: PropTypes.bool.isRequired
+};
 
 export default AppPresenter;
